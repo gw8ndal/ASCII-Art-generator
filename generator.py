741354766249b66661e1f49f35a7f052ad1a36e9
gw8ndal <61947142+gw8ndal@users.noreply.github.com>
2020-03-09 19:43:19 +0100

    From 4 grey variations to 9, better image readability

--- generator.py
+++ generator.py
@@ -4,15 +4,15 @@ import easygui
 from resizeimage import resizeimage
 import webbrowser
 
-print('What resolution should be the final image ?')
-resX = int(input('Resoltion X ='))
-resY = int(input('Resoltion Y ='))
+print("Quelle résolution doit être l'image finale ?")
+
+resX = int(input('Résolution X = '))
+resY = int(input('Résolution Y = '))
 
 image_path = easygui.fileopenbox()
 image_original = Image.open(image_path)
 
-
-image = resizeimage.resize_thumbnail(image_original, [resX, resY]) 
+image = resizeimage.resize_thumbnail(image_original, [resX, resY])
 
 largeur, hauteur = image.size
 
@@ -21,21 +21,36 @@ output = io.StringIO()
 image_nb = image.convert('L')
 
 for y in range(0, hauteur, 2):
-    for x in range(largeur):
-        value = image_nb.getpixel((x, y))
-        if value < 64:
-            output.write('@')
+	for x in range(largeur):
+		value = image_nb.getpixel((x, y))
+		if value < 28:
+			output.write('@')
+    
+		elif value < 56:
+			output.write('%')
+    
+		elif value < 84:
+			output.write('#')
 
-        elif value < 128:
-            output.write('#')
+		elif value < 112:
+			output.write('*')
+    
+		elif value < 140:
+			output.write('+')
 
-        elif value < 192:
-            output.write('/')
-        else :
-            output.write(',')
-    output.write('\n')
+		elif value < 168:
+			output.write('=')
+    
+		elif value < 196:
+			output.write('-')
 
-with open('Final image.txt', mode='w') as f: 
+		elif value < 224:
+			output.write(':')
+		else :
+			output.write('.')
+	output.write('\n')
+
+with open('Final image.txt', mode='w') as f:
     print(output.getvalue(), file=f)
 
 webbrowser.open('Final image.txt')
